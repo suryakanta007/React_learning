0e7cab08a0b77902107c195001b5188c45ffb64f
suryakanta007 <sahusuryakanta073@gmail.com>
2024-07-20 20:33:07 +0530

    understand hooks and useState with a small counter project.

---
+++ 02counter/counter_app/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  let [counter,setCounter] = useState(6)
+
+ let addValue = ()=>{
+  counter++;
+  setCounter(counter);
+  
+ };
+ let removeValue = ()=>{
+  counter--;
+  setCounter(counter);
+ }
+
+
+  return (
+    <>
+    <h1>Chai aur react</h1>
+    <h2>Counter Value: {counter}</h2>
+    <button onClick={addValue}>incriment</button><br/>
+    <button onClick={removeValue}>decriment</button>
+    </>
+  )
+}
+
+export default App
