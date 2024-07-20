6c5053b5fb6daf95d5fedf8902601ea7075f4270
suryakanta007 <sahusuryakanta073@gmail.com>
2024-07-20 08:57:19 +0530

    UnderStand the how react itrnally render the html and javaScript and also know about how we can use the variables in the jsx (evaluate experssion)

--- 01vite_react/src/main.jsx
+++ 01vite_react/src/main.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function Anotherapp(){
+  let username  = "Surya"
+  return (
+    <h1>Discode is the best pleace to discussion {username}</h1>
+  )
+}
+
 
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode>  
+    <Anotherapp/>
   </React.StrictMode>,
 )
