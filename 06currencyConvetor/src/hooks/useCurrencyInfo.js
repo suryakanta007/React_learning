0bbd84336e1de85ef3be2c6d471fae6bc838e2b7
suryakanta007 <sahusuryakanta073@gmail.com>
2024-09-13 12:18:36 +0530

    setup

---
+++ 06currencyConvetor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    let api = ` https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`;
+    fetch(api)
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
